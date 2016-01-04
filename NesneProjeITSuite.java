d0eccb27bc6a615ce176725351a74c1c6d949f45
krtls <kurtulusbeylan@gmail.com>
2016-01-04 23:27:04 +0200

    Create NesneProjeITSuite.java

---
+++ NesneProjeITSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nesneproje;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author beatles
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({nesneproje.KelimeIT.class, nesneproje.NesneProjeIT.class})
+public class NesneprojeITSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
